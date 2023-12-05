d5d9c416d9398da552589077dac2eccb3b1ff057
TzvikaShtainer <TzvikaShtainer@gmail.com>
2023-12-05 09:54:16 +0200

    Adding a Little Level Design & Endless Background

---
+++ 2D RPG Game/Assets/ParallaxBackground.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxBackground : MonoBehaviour
+{
+    private GameObject cam;
+    
+    [SerializeField]private float parallaxEffect;
+    
+    private float xPosition;
+    private float length;
+
+    private void Start()
+    {
+        cam = GameObject.Find("Main Camera");
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
+        xPosition = transform.position.x;
+    }
+
+    private void Update()
+    {
+        float distanceToMove = cam.transform.position.x * parallaxEffect;
+        float distanceMoved = cam.transform.position.x * (1 - parallaxEffect);
+        
+        transform.position = new Vector3(xPosition + distanceToMove, transform.position.y);
+
+        if (distanceToMove > xPosition + length)
+            xPosition = xPosition + length;
+        else if (distanceToMove > xPosition - length)
+            xPosition = xPosition - length;
+    }
+}
