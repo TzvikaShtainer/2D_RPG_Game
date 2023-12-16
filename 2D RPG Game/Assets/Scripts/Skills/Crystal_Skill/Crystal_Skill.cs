846aa567b42cbeb4ea9a4c968ae3f8fdd979cc69
TzvikaShtainer <TzvikaShtainer@gmail.com>
2023-12-16 13:40:14 +0200

    Make The Crystal Destroy Itself After Certain Time

--- 2D RPG Game/Assets/Scripts/Skills/Crystal_Skill/Crystal_Skill.cs
+++ 2D RPG Game/Assets/Scripts/Skills/Crystal_Skill/Crystal_Skill.cs
@@ -8,12 +8,19 @@ public class Crystal_Skill : Skill
     [SerializeField] private GameObject crystalPrefab;
     private GameObject currentCrystal;
 
+    [SerializeField] private float crystalDuration;
+
     public override void UseSkill()
     {
         base.UseSkill();
 
         if (currentCrystal == null)
+        {
             currentCrystal = Instantiate(crystalPrefab, Player.transform.position, Quaternion.identity);
+            Crystal_Skill_Controller currentCrystalScript = currentCrystal.GetComponent<Crystal_Skill_Controller>();
+            
+            currentCrystalScript.SetupCrystal(crystalDuration);
+        }
         else
         {
             Player.transform.position = currentCrystal.transform.position;
--- 2D RPG Game/Assets/Scripts/Skills/Crystal_Skill/Crystal_Skill_Controller.cs
+++ 2D RPG Game/Assets/Scripts/Skills/Crystal_Skill/Crystal_Skill_Controller.cs
@@ -1,8 +1,26 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Crystal_Skill_Controller : MonoBehaviour
 {
-    
+    private float crystalExistTimer;
+
+    public void SetupCrystal(float crystalDuration)
+    {
+        crystalExistTimer = crystalDuration;
+    }
+
+    private void Update()
+    {
+        crystalExistTimer -= Time.deltaTime;
+
+        if (crystalExistTimer < 0)
+        {
+            SelfDestroy();
+        }
+    }
+
+    private void SelfDestroy() => Destroy(gameObject);
 }
