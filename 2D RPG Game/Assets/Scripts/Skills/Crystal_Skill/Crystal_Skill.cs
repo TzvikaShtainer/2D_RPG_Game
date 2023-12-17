59fc5337451be2f0893c22bdda6b9d35409915f4
TzvikaShtainer <TzvikaShtainer@gmail.com>
2023-12-17 08:16:38 +0200

    Create Multi Crystal Ability

--- 2D RPG Game/Assets/Scripts/Skills/Crystal_Skill/Crystal_Skill.cs
+++ 2D RPG Game/Assets/Scripts/Skills/Crystal_Skill/Crystal_Skill.cs
@@ -16,10 +16,19 @@ public class Crystal_Skill : Skill
     [SerializeField] private bool canMoveToEnemy;
     [SerializeField] private float moveSpeed;
 
+    [Header("Multi Stacking Crystal")] 
+    [SerializeField] private bool canUseMultiStacks;
+    [SerializeField] private float amountOfCrystals;
+    [SerializeField] private float multiCrystalCooldown;
+    [SerializeField] private List<GameObject> crystalsLeft = new List<GameObject>();
+
     public override void UseSkill()
     {
         base.UseSkill();
 
+        if(CanUseMultiCrystals())
+            return;
+        
         if (currentCrystal == null)
         {
             currentCrystal = Instantiate(crystalPrefab, Player.transform.position, Quaternion.identity);
@@ -43,5 +52,39 @@ public class Crystal_Skill : Skill
         Vector2 playerPos = Player.transform.position;
         Player.transform.position = currentCrystal.transform.position;
         currentCrystal.transform.position = playerPos;
+    }
+
+    private bool CanUseMultiCrystals()
+    {
+        if (canUseMultiStacks)
+        {
+            if (crystalsLeft.Count > 0)
+            {
+                GameObject crystalToSpawn = crystalsLeft[crystalsLeft.Count - 1];
+                GameObject newCrystal = Instantiate(crystalToSpawn, Player.transform.position, Quaternion.identity);
+                
+                crystalsLeft.Remove(crystalToSpawn);
+                
+                newCrystal.GetComponent<Crystal_Skill_Controller>().
+                    SetupCrystal(crystalDuration, canExplode, canMoveToEnemy, moveSpeed, FindClosestEnemy(newCrystal.transform));
+
+                if (crystalsLeft.Count <= 0)
+                {
+                    //colldown skill
+                    //refill Crystals
+                }
+            }
+            
+            return true;
+        }
+
+        return false;
+    }
+    private void RefillCrystals()
+    {
+        for (int i = 0; i < amountOfCrystals; i++)
+        {
+            crystalsLeft.Add(crystalPrefab);
+        }
     }
 }
