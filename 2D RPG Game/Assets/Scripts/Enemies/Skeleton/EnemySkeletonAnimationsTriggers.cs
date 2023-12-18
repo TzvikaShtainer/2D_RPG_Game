5e8c108fd2911e5ab5ad86a8592791209280ea5f
TzvikaShtainer <TzvikaShtainer@gmail.com>
2023-12-18 13:14:02 +0200

    enemy Can Damage The Player

--- 2D RPG Game/Assets/Scripts/Enemies/Skeleton/EnemySkeletonAnimationsTriggers.cs
+++ 2D RPG Game/Assets/Scripts/Enemies/Skeleton/EnemySkeletonAnimationsTriggers.cs
@@ -19,7 +19,11 @@ public class EnemySkeletonAnimationsTriggers : MonoBehaviour
         foreach (var hit in colliders)
         {
             if (hit.GetComponent<Player>() != null)
+            {
                 hit.GetComponent<Player>().Damage();
+                PlayerStats target = hit.GetComponent<PlayerStats>();
+                EnemySkeleton.Stats.DoDamage(target);
+            }
         }
     }
 
