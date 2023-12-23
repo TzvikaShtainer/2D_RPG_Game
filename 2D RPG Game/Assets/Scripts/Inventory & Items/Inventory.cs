d71b5f2f564e656ec06fb301d28f43647232f499
TzvikaShtainer <TzvikaShtainer@gmail.com>
2023-12-23 12:40:54 +0200

    Add Stash Inventory For Materials

--- 2D RPG Game/Assets/Scripts/Inventory & Items/Inventory.cs
+++ 2D RPG Game/Assets/Scripts/Inventory & Items/Inventory.cs
@@ -8,14 +8,24 @@ public class Inventory : MonoBehaviour //one inventory that contains a list of i
 {
     public static Inventory instance;
 
-    public List<InventoryItem> inventoryItems; 
+    [Header("Inventory")] 
+    public List<InventoryItem> inventory; 
     public Dictionary<ItemData, InventoryItem> inventoryDictionary;
+    
+    [Header("Stash")] 
+    public List<InventoryItem> stash;
+    public Dictionary<ItemData, InventoryItem> stashDictionary;
 
 
     [Header("Inventory UI")] 
     [SerializeField] private Transform inventorySlotParent;
-    private UI_ItemSlot[] itemSlot; 
-
+    private UI_ItemSlot[] inventoryItemSlot; 
+    
+    
+    [Header("Stash UI")] 
+    [SerializeField] private Transform stashSlotParent;
+    private UI_ItemSlot[] stashItemSlot; 
+    
     private void Awake()
     {
         if (instance == null)
@@ -26,21 +36,39 @@ public class Inventory : MonoBehaviour //one inventory that contains a list of i
 
     private void Start()
     {
-        inventoryItems = new List<InventoryItem>();
+        inventory = new List<InventoryItem>();
         inventoryDictionary = new Dictionary<ItemData, InventoryItem>();
-
-        itemSlot = inventorySlotParent.GetComponentsInChildren<UI_ItemSlot>();
+        inventoryItemSlot = inventorySlotParent.GetComponentsInChildren<UI_ItemSlot>();
+        
+        stash = new List<InventoryItem>();
+        stashDictionary = new Dictionary<ItemData, InventoryItem>();
+        stashItemSlot = stashSlotParent.GetComponentsInChildren<UI_ItemSlot>();
     }
 
     private void UpdateSlotUI()
     {
-        for (int i = 0; i < inventoryItems.Count; i++)
+        for (int i = 0; i < inventory.Count; i++)
         {
-            itemSlot[i].UpdateSlot(inventoryItems[i]);
+            inventoryItemSlot[i].UpdateSlot(inventory[i]);
+        }
+        
+        for (int i = 0; i < stash.Count; i++)
+        {
+            stashItemSlot[i].UpdateSlot(stash[i]);
         }
     }
 
     public void AddItem(ItemData _item)
+    {
+        if (_item.itemType == ItemType.Equipment)
+            AddItem(_item, inventory, inventoryDictionary);
+        else if (_item.itemType == ItemType.Material)
+            AddItem(_item, stash, stashDictionary);
+        
+        UpdateSlotUI();
+    }
+
+    private void AddToInventory(ItemData _item)
     {
         if (inventoryDictionary.TryGetValue(_item, out InventoryItem value))
         {
@@ -49,24 +77,60 @@ public class Inventory : MonoBehaviour //one inventory that contains a list of i
         else
         {
             InventoryItem newItem = new InventoryItem(_item);
-            inventoryItems.Add(newItem);
+            inventory.Add(newItem);
             inventoryDictionary.Add(_item, newItem);
         }
-        
-        UpdateSlotUI();
+    }
+    
+    private void AddToStash(ItemData _item)
+    {
+        if (stashDictionary.TryGetValue(_item, out InventoryItem value))
+        {
+            value.AddStack();
+        }
+        else
+        {
+            InventoryItem newItem = new InventoryItem(_item);
+            stash.Add(newItem);
+            stashDictionary.Add(_item, newItem);
+        }
     }
 
+    private void AddItem(ItemData _item, List<InventoryItem> addToList, Dictionary<ItemData, InventoryItem> addToDict)
+    {
+        if (addToDict.TryGetValue(_item, out InventoryItem value))
+        {
+            value.AddStack();
+        }
+        else
+        {
+            InventoryItem newItem = new InventoryItem(_item);
+            addToList.Add(newItem);
+            addToDict.Add(_item, newItem);
+        }
+    }
     public void RemoveItem(ItemData itemToRemove)
     {
-        if (inventoryDictionary.TryGetValue(itemToRemove, out InventoryItem value))
+        if (inventoryDictionary.TryGetValue(itemToRemove, out InventoryItem inventoryValue))
         {
-            if (value.stackSize <= 1)
+            if (inventoryValue.stackSize <= 1)
             {
-                inventoryItems.Remove(value);
+                inventory.Remove(inventoryValue);
                 inventoryDictionary.Remove(itemToRemove);
             }
             else
-                value.RemoveStack();
+                inventoryValue.RemoveStack();
+        }
+        
+        if (inventoryDictionary.TryGetValue(itemToRemove, out InventoryItem stashValue))
+        {
+            if (stashValue.stackSize <= 1)
+            {
+                stash.Remove(stashValue);
+                stashDictionary.Remove(itemToRemove);
+            }
+            else
+                stashValue.RemoveStack();
         }
         
         UpdateSlotUI();
